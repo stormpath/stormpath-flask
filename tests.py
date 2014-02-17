1a7e72126a1a5aacda8dae4fc7d75e3a41e52ca5
Randall Degges <r@rdegges.com>
2014-02-17 09:35:44 -0800

    Adding a basic tests.py skeleton.
    This is where we'll have our tests / etc.

---
+++ tests.py
@@ -0,0 +1,7 @@
+"""
+Our Flask-Stormpath tests.
+
+Why are these in a single file instead of a directory?  Honestly, it's because
+this extension is so simple, it didn't warrant a proper directory / module.  So
+we'll just follow Flask conventions and have single file stuff going on.
+"""
