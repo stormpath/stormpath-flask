dc05e830342c89815e8796d8bc5ddbe0d62803b6
Randall Degges <r@rdegges.com>
2014-02-18 09:25:10 -0800

    Adding proper author.

--- flask_stormpath.py
+++ flask_stormpath.py
@@ -18,7 +18,7 @@
 
 __version_info__ = ('0', '0', '1')
 __version__ = '.'.join(__version_info__)
-__author__ = 'Randall Degges'
+__author__ = 'Stormpath, Inc.'
 __license__ = 'Apache'
 __copyright__ = '(c) 2012 - 2014 Stormpath, Inc.'
 
