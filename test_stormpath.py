59c6ec24d3dbc90fafefec17916f5747d67e02b1
Randall Degges <r@rdegges.com>
2014-02-25 19:53:33 -0800

    Adding tests for our 'groups_required' decorator.

--- test_stormpath.py
+++ test_stormpath.py
@@ -14,12 +14,17 @@ from os import (
 from unittest import TestCase
 from uuid import uuid4
 
-from flask import Flask
+from flask import (
+    Flask,
+    request,
+)
 from flask.ext.stormpath import (
     StormpathManager,
     User,
     _user_context_processor,
+    groups_required,
     login_user,
+    logout_user,
 )
 
 from stormpath.client import Client
@@ -245,5 +250,146 @@ class TestStormpathManager(TestCase):
 
     def tearDown(self):
         remove('apiKey.properties')
+        self.application.delete()
+        self.client.directories.search(self.application_name)[0].delete()
+
+
+class TestGroupsRequired(TestCase):
+    """Our groups_required decorator test suite."""
+
+    def setUp(self):
+        self.client = Client(
+            id = environ.get('STORMPATH_API_KEY_ID'),
+            secret = environ.get('STORMPATH_API_KEY_SECRET'),
+        )
+        self.application_name = 'flask-stormpath-tests-%s' % uuid4().hex
+
+        # Try to delete our test application / directory first, this way if we
+        # mess something up while developing test code (like I have many times
+        # now), we won't get issues and have to manually remove these resources.
+        try:
+            self.client.applications.search(self.application_name)[0].delete()
+            self.client.directories.search(self.application_name)[0].delete()
+        except:
+            pass
+
+        self.application = self.client.applications.create({
+            'name': self.application_name,
+            'description': 'This application is ONLY used for testing the Flask-Stormpath library. Please do not use this for anything serious.',
+        }, create_directory=True)
+
+        self.user = self.application.accounts.create({
+            'given_name': 'Randall',
+            'surname': 'Degges',
+            'email': 'randall@stormpath.com',
+            'username': 'randall',
+            'password': 'woot1LoveCookies!',
+        })
+        self.user.__class__ = User
+
+        self.admin_group = self.application.groups.create({
+            'name': 'admins',
+        })
+        self.developers_group = self.application.groups.create({
+            'name': 'developers',
+        })
+
+        self.app = Flask(__name__)
+        self.app.config['SECRET_KEY'] = 'woot'
+        self.app.config['STORMPATH_API_KEY_ID'] = environ.get('STORMPATH_API_KEY_ID')
+        self.app.config['STORMPATH_API_KEY_SECRET'] = environ.get('STORMPATH_API_KEY_SECRET')
+        self.app.config['STORMPATH_APPLICATION'] = self.application_name
+
+        StormpathManager(self.app)
+
+    def test_unauthenticated(self):
+        with self.app.test_client() as c:
+            @self.app.route('/')
+            @groups_required(['admins'])
+            def test_view():
+                return 'hi'
+
+            self.assertEqual(c.get('/').status_code, 401)
+
+    def test_unauthorized(self):
+        with self.app.test_client() as c:
+
+            @self.app.route('/login', methods=['POST'])
+            def login():
+                user = User.from_login(
+                    request.form.get('email'),
+                    request.form.get('password'),
+                )
+                login_user(user, remember=True)
+                return 'logged in'
+
+            @self.app.route('/')
+            @groups_required(['admins'])
+            def test_view():
+                return 'hi'
+
+            resp = c.post('/login', data=dict(
+                email = self.user.email,
+                password = 'woot1LoveCookies!',
+            ))
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertEqual(c.get('/').status_code, 401)
+
+    def test_authorized(self):
+        with self.app.test_client() as c:
+
+            @self.app.route('/login', methods=['POST'])
+            def login():
+                user = User.from_login(
+                    request.form.get('email'),
+                    request.form.get('password'),
+                )
+                login_user(user, remember=True)
+                return 'logged in'
+
+            @self.app.route('/logout')
+            def logout():
+                logout_user()
+
+            @self.app.route('/')
+            @groups_required(['admins'])
+            def test_view():
+                return 'hi'
+
+            @self.app.route('/another_test')
+            @groups_required(['admins', 'developers'])
+            def another_test_view():
+                return 'hi'
+
+            # Add the user to the developers group.
+            self.developers_group.add_account(self.user)
+
+            resp = c.post('/login', data=dict(
+                email = self.user.email,
+                password = 'woot1LoveCookies!',
+            ))
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertEqual(c.get('/').status_code, 401)
+            self.assertEqual(c.get('/another_test').status_code, 401)
+
+            # Force a logout to clear the user's data.
+            c.get('/logout')
+
+            # Add the user to the admins group.
+            self.admin_group.add_account(self.user)
+
+            # Force a login to reset the user.
+            resp = c.post('/login', data=dict(
+                email = self.user.email,
+                password = 'woot1LoveCookies!',
+            ))
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertEqual(c.get('/').status_code, 200)
+            self.assertEqual(c.get('/another_test').status_code, 200)
+
+    def tearDown(self):
         self.application.delete()
         self.client.directories.search(self.application_name)[0].delete()
