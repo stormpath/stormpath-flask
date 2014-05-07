463386417fefb90a1bbf34726cf2a7ddc6d87050
Randall Degges <r@rdegges.com>
2014-05-07 01:32:57 -0500

    Adding new helper function, bootstrap_flask_app.
    This simplifies our tests.

--- tests/helpers.py
+++ tests/helpers.py
@@ -9,6 +9,8 @@ operations.
 from os import environ
 from uuid import uuid4
 
+from flask import Flask
+from flask.ext.stormpath import StormpathManager
 from stormpath.client import Client
 
 
@@ -36,6 +38,7 @@ def bootstrap_app(client):
         This will *also* create a Stormpath directory of the same name, so that
         you can use this application to create users immediately.
 
+    :param obj client: A Stormpath Client resource.
     :rtype: obj
     :returns: A new Stormpath Application, fully initialized.
     """
@@ -43,3 +46,23 @@ def bootstrap_app(client):
         'name': 'flask-stormpath-tests-%s' % uuid4().hex,
         'description': 'This application is ONLY used for testing the Flask-Stormpath library. Please do not use this for anything serious.',
     }, create_directory=True)
+
+
+def bootstrap_flask_app(app):
+    """
+    Create a new, fully initialized Flask app.
+
+    :param obj app: A Stormpath Application resource.
+    :rtype: obj
+    :returns: A new Flask app.
+    """
+    a = Flask(__name__)
+    a.config['DEBUG'] = True
+    a.config['SECRET_KEY'] = uuid4().hex
+    a.config['STORMPATH_API_KEY_ID'] = environ.get('STORMPATH_API_KEY_ID')
+    a.config['STORMPATH_API_KEY_SECRET'] = environ.get('STORMPATH_API_KEY_SECRET')
+    a.config['STORMPATH_APPLICATION'] = app.name
+    a.config['WTF_CSRF_ENABLED'] = False
+    StormpathManager(a)
+
+    return a
