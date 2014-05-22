7c6e114e255e28f277bbdcd421a940ed5032a861
Randall Degges <r@rdegges.com>
2014-05-22 13:23:39 -0700

    Version bump for stormpath sdk.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         'Flask-WTF>=0.9.5',
         'facebook-sdk==0.4.0',
         'oauth2client==1.2',
-        'stormpath==1.2.3',
+        'stormpath==1.2.4',
     ],
     classifiers = [
         'Environment :: Web Environment',
