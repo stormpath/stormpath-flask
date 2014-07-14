848f8b789c335acda8c7e196ab15304dfcfdb11e
Randall Degges <r@rdegges.com>
2014-07-14 14:20:30 -0700

    Adding a ChangePasswordForm.
    This validates that two password are equal.

--- flask_stormpath/forms.py
+++ flask_stormpath/forms.py
@@ -3,7 +3,7 @@
 
 from flask.ext.wtf import Form
 from wtforms.fields import PasswordField, StringField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, ValidationError
 
 
 class RegistrationForm(Form):
@@ -60,3 +60,23 @@ class ForgotPasswordForm(Form):
     This class is used to retrieve a user's email address.
     """
     email = StringField('Email', validators=[InputRequired()])
+
+
+class ChangePasswordForm(Form):
+    """
+    Change a user's password.
+
+    This class is used to retrieve a user's password twice to ensure it's valid
+    before making a change.
+    """
+    password = PasswordField('Password', validators=[InputRequired()])
+    password_again = PasswordField('Password (again)', validators=[InputRequired()])
+
+    def validate_password_again(self, field):
+        """
+        Ensure both password fields match, otherwise raise a ValidationError.
+
+        :raises: ValidationError if passwords don't match.
+        """
+        if self.password.data != field.data:
+            raise ValidationError("Passwords don't match.")
