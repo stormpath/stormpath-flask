4a67cbe9adab2f33d61c50c8f41a3d3fec2edb9c
Randall Degges <r@rdegges.com>
2015-01-27 14:39:14 -0800

    Bump.

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Flask-Stormpath'
-copyright = u'2014, Stormpath, Inc.'
+copyright = u'2015, Stormpath, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
