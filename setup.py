49588e08825e9040c1e6aa02f83594c26bbd1a63
Randall Degges <r@rdegges.com>
2015-01-28 10:02:27 -0800

    Updating our Storpmath dependency.
    This fixes a lot of bugs, and speeds things up quite a bit with our caching
    updates.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         'Flask-WTF>=0.9.5',
         'facebook-sdk==0.4.0',
         'oauth2client==1.2',
-        'stormpath==1.2.7',
+        'stormpath==1.3.2',
     ],
     classifiers = [
         'Environment :: Web Environment',
