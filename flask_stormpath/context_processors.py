f6d2b72adde0c792b1a7d7913a9778762dbbad2d
Randall Degges <r@rdegges.com>
2015-02-11 14:03:51 -0800

    Adding a new context processor. Fixes #23.
    This commit modifies our base template, allowing users to dynamically specify
    whatever base template they want!

--- flask_stormpath/context_processors.py
+++ flask_stormpath/context_processors.py
@@ -1,6 +1,7 @@
 """Custom context processors to make template development simpler."""
 
 
+from flask import current_app
 from flask.ext.login import _get_user
 
 
@@ -24,3 +25,26 @@ def user_context_processor():
     https://github.com/stormpath/stormpath-sdk-python
     """
     return {'user': _get_user()}
+
+
+def inject_base_template():
+    """
+    Insert a special variable named `stormpath_base_template` into all
+    templates.
+
+    This makes it possible for developers to create their own base template,
+    from which all other templates extend.  This is not possible normally, as
+    the Jinja2 `extends` tag doesn't allow for dictionary lookups :(
+
+    With this, you can now write templates that start off like this::
+
+        {% extends stormpath_base_template %}
+
+    And then you can control what template is the base template by setting the
+    `STORMPATH_BASE_TEMPLATE` setting via your Flask config::
+
+        app.config['STORMPATH_BASE_TEMPLATE'] = 'my_custom_base_template.html'
+
+    This makes writing your own authentication pages much simpler.
+    """
+    return {'stormpath_base_template': current_app.config['STORMPATH_BASE_TEMPLATE']}
