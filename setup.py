3319788802ce772a4c3cd6d80487a79fbde6243c
Randall Degges <r@rdegges.com>
2015-03-02 16:22:07 -0800

    Version bump.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class RunTests(Command):
 
 setup(
     name = 'Flask-Stormpath',
-    version = '0.3.6',
+    version = '0.3.7',
     url = 'https://github.com/stormpath/stormpath-flask',
     license = 'Apache',
     author = 'Stormpath, Inc.',
