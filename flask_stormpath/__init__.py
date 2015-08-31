f4fe56d8f2ee9a65c83310306178407fe75ba70c
Randall Degges <r@rdegges.com>
2015-08-31 11:33:02 -0700

    Version bump.

--- flask_stormpath/__init__.py
+++ flask_stormpath/__init__.py
@@ -15,7 +15,7 @@
 """
 
 
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 __version_info__ = __version__.split('.')
 __author__ = 'Stormpath, Inc.'
 __license__ = 'Apache'
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class RunTests(Command):
 
 setup(
     name = 'Flask-Stormpath',
-    version = '0.4.3',
+    version = '0.4.4',
     url = 'https://github.com/stormpath/stormpath-flask',
     license = 'Apache',
     author = 'Stormpath, Inc.',
