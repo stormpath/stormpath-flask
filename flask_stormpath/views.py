3503fc89525fcd48738f491ff55d30eb7822c12a
Paul Durivage <pauldurivage@gmail.com>
2015-08-31 12:49:51 -0500

    Add Verify Email message flashing

--- flask_stormpath/views.py
+++ flask_stormpath/views.py
@@ -83,6 +83,11 @@ def register():
                 # STORMPATH_REDIRECT_URL setting.
                 login_user(account, remember=True)
 
+                # The email address must be verified, so pop an alert about it.
+                if current_app.config['STORMPATH_VERIFY_EMAIL'] is True:
+                    flash('You must validate your email address before logging '
+                          'in. Please check your email for instructions.')
+
                 if 'STORMPATH_REGISTRATION_REDIRECT_URL'\
                         in current_app.config:
                     redirect_url = current_app.config[
