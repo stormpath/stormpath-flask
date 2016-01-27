52b5258bce075a15fd9f4d58343d1179e38e9d59
Randall Degges <r@rdegges.com>
2016-01-27 14:24:31 -0800

    Adding trove classifers.

--- setup.py
+++ setup.py
@@ -73,5 +73,12 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Session',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
